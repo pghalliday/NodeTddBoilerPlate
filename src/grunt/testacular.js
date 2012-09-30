593f55da74cf16010c32c6fe94adef1b8e9685f0
Peter Halliday <pghalliday@gmail.com>
2012-10-01 00:20:49 +0200

    Mocha and testacular working. Need to sort out lint

---
+++ src/grunt/testacular.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  var testacular = require('testacular');
+
+  grunt.registerMultiTask('testacular', 'Run browser unit tests with Testacular', function() {
+    // Tell grunt this task is asynchronous.
+    var done = this.async();
+
+    // spawn the testacular server
+    grunt.utils.spawn({
+      cmd: 'node_modules/.bin/testacular',
+      args: ['start', this.file.src, '--single-run']
+    }, function(error, result, code) {
+      console.log(result.stdout);
+      console.log(result.stderr);
+      done(code === 0);
+    });
+  });
+};
---
+++ src/server/my.test.js
@@ -0,0 +1,8 @@
+var should = require('chai').should();
+
+describe('test', function() {
+  it('should pass', function() {
+    true.should.be.true;
+  });  
+});
+
